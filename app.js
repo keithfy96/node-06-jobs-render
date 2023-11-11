59edfdfddf1051c4f4354aad6c18d87614c3f392
keithlim <keithlimek@gmail.com>
2023-11-11 21:44:08 +0800

    added swagger docs

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const express = require("express");
 const app = express();
@@ -40,8 +46,9 @@ console.log("all middleware loaded - self");
 
 // routes
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send(`<h1>jobs API</h1><a href="/api-docs">Documentation</a>`);
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
