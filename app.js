252e9a6ffbecfb6cfa813c89871022d0866fb4d6
keithlim <keithlimek@gmail.com>
2023-11-11 19:25:53 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+const app = express();
+
+console.log("app started - self");
+// connectDB
+const connectDB = require("./db/connect");
+
+// authentication
+const authenticateUser = require("./middleware/authentication");
+
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// extra packages
+console.log("all middleware loaded - self");
+
+// routes
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+console.log("app start");
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("db connected");
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
